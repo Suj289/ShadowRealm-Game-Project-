d31f55c323a1caf26274a2a1b91e6ee1b6c224a5
Suj289 <macslup03@gmail.com>
2025-12-28 15:52:04 +0200

    Fix deployment config for Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",   // ensure correct asset loading
+  base: "/",        // important for correct asset loading
   plugins: [react()]
 });
